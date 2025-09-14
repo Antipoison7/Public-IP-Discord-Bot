f8f7c7fb4c4ef1808da94a2a8d2784d7cdbccd04
Antipoison7 <75201302+Antipoison7@users.noreply.github.com>
2025-09-14 17:39:48 +1000

    Updated the bot to use discord slash commands

--- ip_bot.py
+++ ip_bot.py
@@ -11,46 +11,47 @@ api_key = os.getenv("API_KEY")
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!",intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
 
 @bot.event
 async def on_ready():
     print('Started the bot')
+    await bot.tree.sync()
     check_ip_changed.start()
 
-@bot.command()
-async def feed(ctx):
-    await ctx.send("Why are you trying to feed me, I'm a DNS server")
+@bot.tree.command(name="feed", description="Feed the bot")
+async def feed(interaction: discord.Interaction):
+    await interaction.response.send_message("Why are you trying to feed me, I'm a DNS server")
     
 
-@bot.command()
-async def unfeed(ctx):
-    await ctx.send("Bruh")
+@bot.tree.command(name="unfeed", description="Unfeed the bot")
+async def unfeed(interaction: discord.Interaction):
+    await interaction.response.send_message("Bruh")
 
-@bot.command()
-async def set_notifs(ctx):
-    channel_id = ctx.channel.id
+@bot.tree.command(name="set_notifs", description="Sets the channel that the automated change notifications are sent to")
+async def set_notifs(interaction: discord.Interaction):
+    channel_id = interaction.channel.id
     with open("notifs_channel.txt", "w") as file:
         file.write(str(channel_id))
-        await ctx.send(f"The IP notifications has been set to channel: {str(channel_id)}")
+        await interaction.response.send_message(f"The IP notifications has been set to channel: {str(channel_id)}")
 
-@bot.command()
-async def public(ctx):
+@bot.tree.command(name="public_ip", description="Outputs the current public IP address")
+async def public_ip(interaction: discord.Interaction):
     try:
         response = requests.get("https://ipinfo.io/ip", timeout=10)
 
         ip_string = response.content.decode('utf-8')
 
         if response.status_code == 404:
-            await ctx.send("404 Error, I didn't even know this could happen")
+            await interaction.response.send_message("404 Error, I didn't even know this could happen")
         else:
             with open("prev_ip.txt", "w") as file:
                 file.write(str(ip_string))
             
-            await ctx.send(f"Public IP: `{ip_string}`")
+            await interaction.response.send_message(f"Public IP: `{ip_string}`")
 
     except requests.Timeout:
-        await ctx.send("Request Timed Out")
+        await interaction.response.send_message("Request Timed Out")
 
 @tasks.loop(minutes=10.0)
 async def check_ip_changed():
